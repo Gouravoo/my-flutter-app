41526ab43bf651b9c440df4e82b23686a972b1ae
Builder <builder@local>
2026-07-10 13:45:26 +0530

    fix: add Zego navigatorKey and useSystemCallingUI for call interfaces and offline push

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
 import 'screens/splash_screen.dart';
 import 'core/theme.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 /// Handle background FCM messages (required for Firebase Messaging)
 @pragma('vm:entry-point')
@@ -37,6 +41,11 @@ void main() async {
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6Y2thbm1mZ2tjbGp2c3JvYW1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwMjgxMzIsImV4cCI6MjA5ODYwNDEzMn0.ynKAmkCD2sTr4N62uhuB-r_OND0nnQJokSCbVWUqXpE',
   );
 
+  ZegoUIKitPrebuiltCallInvitationService().setNavigatorKey(navigatorKey);
+  ZegoUIKitPrebuiltCallInvitationService().useSystemCallingUI(
+    [ZegoUIKitSignalingPlugin()],
+  );
+
   runApp(const AarogyaPlusApp());
 }
 
@@ -48,6 +57,7 @@ class AarogyaPlusApp extends StatelessWidget {
     return MaterialApp(
       title: 'AarogyaPlus',
       debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
